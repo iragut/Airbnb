d965f6f5c5f409638a463e7c837ff195479e4f54
iragut <74363823+iragut@users.noreply.github.com>
2025-07-08 00:19:23 +0300

    Add register and login page

--- main.go
+++ main.go
@@ -20,10 +20,41 @@ func main_page_handler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func login_handler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	tmpl := template.Must(template.ParseFiles("template/login_page.html"))
+	err := tmpl.Execute(w, nil)
+	if err != nil {
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		log.Println("Error executing template:", err)
+	}
+}
+
+func register_handler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	tmpl := template.Must(template.ParseFiles("template/register_page.html"))
+	err := tmpl.Execute(w, nil)
+	if err != nil {
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		log.Println("Error executing template:", err)
+	}
+}
+
 func main() {
 	fs := http.FileServer(http.Dir("static/"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
 	http.HandleFunc("/", main_page_handler)
+	http.HandleFunc("/login", login_handler)
+	http.HandleFunc("/register", register_handler)
 
 	log.Println("Server starting on :8080")
 	http.ListenAndServe(":8080", nil)
